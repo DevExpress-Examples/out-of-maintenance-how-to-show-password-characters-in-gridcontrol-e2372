054d3fdceab7d9c224041a7b92a6ccee32e9019d
DevExpressExampleBot <fakeEmail>
2022-12-17 12:41:32 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/Window1.xaml.vb
+++ VB/WpfApplication1/Window1.xaml.vb
@@ -1,64 +1,40 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports DevExpress.Xpo
 Imports System.ComponentModel
-Imports DevExpress.Xpf.Grid
-Imports System.Windows.Threading
-Imports DevExpress.Xpf.Editors.Settings
-Imports System.Threading
-Imports DevExpress.Data
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
 
-		Private coll As List(Of ContactItem)
-		Public Sub New()
-			InitializeComponent()
-			coll = New List(Of ContactItem)()
-			Dim f1 As New ContactItem()
-			f1.Name = "Abcd"
-			f1.Salary = 13.40D
-			f1.Date = DateTime.Now
-			coll.Add(f1)
-			Dim f2 As New ContactItem()
-			f2.Name = "bla1"
-			f2.Date = DateTime.Now
-			f2.Salary = 13
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-			coll.Add(f2)
+        Private coll As List(Of ContactItem)
 
-			Dim f3 As New ContactItem()
-			f3.Name = "la"
-			f3.Salary = 13
-
-			coll.Add(f3)
-
-			Dim f4 As New ContactItem()
-			f4.Name = " bla"
-			f4.Salary = 13
-
-			coll.Add(f4)
-
-			gridControl1.ItemsSource = coll
-
-
-		End Sub
-	End Class
-
-
+        Public Sub New()
+            Me.InitializeComponent()
+            coll = New List(Of ContactItem)()
+            Dim f1 As ContactItem = New ContactItem()
+            f1.Name = "Abcd"
+            f1.Salary = 13.40D
+            f1.Date = Date.Now
+            coll.Add(f1)
+            Dim f2 As ContactItem = New ContactItem()
+            f2.Name = "bla1"
+            f2.Date = Date.Now
+            f2.Salary = 13
+            coll.Add(f2)
+            Dim f3 As ContactItem = New ContactItem()
+            f3.Name = "la"
+            f3.Salary = 13
+            coll.Add(f3)
+            Dim f4 As ContactItem = New ContactItem()
+            f4.Name = " bla"
+            f4.Salary = 13
+            coll.Add(f4)
+            Me.gridControl1.ItemsSource = coll
+        End Sub
+    End Class
 End Namespace
